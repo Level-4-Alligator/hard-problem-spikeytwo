e4188eeb7e4a63da01fcaa1701098c6edd4302cd
Liam Tan <league@WTS21.attlocal.net>
2017-05-25 18:28:56 -0700

    ransom

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -1,13 +1,40 @@
 import java.util.*;
 
 public class RansomNote {
-	Map<String, Integer> magazineMap;
-	Map<String, Integer> noteMap;
+	Map<Integer, String> magazineMap;
+	Map<Integer, String> noteMap;
+
+	String mag;
+	String not;
 
 	public RansomNote(String magazine, String note) {
+		mag = magazine;
+		not = note;
+		magazineMap = new HashMap();
+		noteMap = new HashMap();
 	}
 
 	public boolean solve() {
+		boolean ransom = true;
+		String[] magazine = mag.split(" ");
+		String[] note = not.split(" ");
+		for (int i = 0; i < magazine.length; i++) {
+			magazineMap.put(i, magazine[i]);
+		}
+		for (int i = 0; i < note.length; i++) {
+			noteMap.put(i, note[i]);
+		}
+		for (int i = 0; i < note.length; i++) {
+			if (magazineMap.get(i).equals(noteMap.get(i))) {
+				ransom = true;
+			}
+			if (magazineMap.containsValue(note[i])) {
+				ransom = true;
+			} else {
+				ransom = false;
+			}
+		}
+		return ransom;
 	}
 
 	public static void main(String[] args) {
